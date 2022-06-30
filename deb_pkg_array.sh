7706f40c51444d9924006c325fa69912f5ff8284
bitsko <26992310+bitsko@users.noreply.github.com>
2022-06-30 05:27:28 -0700

    Create deb_pkg_array.sh

---
+++ deb_pkg_array.sh
@@ -0,0 +1,11 @@
+sudo apt update
+declare -a deb_pkg_array=( build-essential git cmake pkg-config \
+        libtool autoconf autogen automake libboost-chrono-dev wget ncat \
+        libminiupnpc-dev libssl-dev libzmq3-dev help2man ninja-build libdb-dev \
+        libdb++-dev libqrencode-dev libcurl4-openssl-dev libncurses-dev python3 \
+        libboost-filesystem-dev libboost-test-dev libboost-thread-dev libevent-dev )
+while read -r line; do
+        if ! dpkg -s "$line" &> /dev/null
+                then sudo apt install "$line"
+        fi
+done <<<$(printf '%s\n' "${deb_pkg_array[@]}")
