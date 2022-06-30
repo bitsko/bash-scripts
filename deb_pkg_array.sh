09cb00f2978f6ecb745ea69316f6783c314bdd8c
bitsko <26992310+bitsko@users.noreply.github.com>
2022-06-30 05:27:58 -0700

    Update deb_pkg_array.sh

--- deb_pkg_array.sh
+++ deb_pkg_array.sh
@@ -1,6 +1,6 @@
 sudo apt update
 declare -a deb_pkg_array=( build-essential git cmake pkg-config \
-        libtool autoconf autogen automake libboost-chrono-dev wget ncat \
+        libtool autoconf autogen automake libboost-chrono-dev wget \
         libminiupnpc-dev libssl-dev libzmq3-dev help2man ninja-build libdb-dev \
         libdb++-dev libqrencode-dev libcurl4-openssl-dev libncurses-dev python3 \
         libboost-filesystem-dev libboost-test-dev libboost-thread-dev libevent-dev )
