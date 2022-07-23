9413046b36725113329ddfca70f9ef8a3260b4a9
bitsko <26992310+bitsko@users.noreply.github.com>
2022-07-23 04:34:53 -0700

    Update wget_show_progress_flag.sh

--- wget_show_progress_flag.sh
+++ wget_show_progress_flag.sh
@@ -5,3 +5,9 @@ if (( $(echo "$wget_version >= 1.16" | bc -l) )); then
 else
 	echo "wget does not work with --show-progress"
 fi
+cmake_version=$(cmake -version | cut -d ' ' -f 3 | sed 's/\.//g' | cut -c -3)
+if (( $(echo "$cmake_version < 313" | bc -l) )); then
+	echo "CMake 3.13 or higher is required. You are running version $(cmake -version | cut -d ' ' -f 3)"
+else 
+	echo "CMake version $(cmake -version | cut -d ' ' -f 3) installed"
+fi
