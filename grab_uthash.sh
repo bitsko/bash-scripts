3e27220561c9fbaaf13825edda511608db18a8b8
bitsko <26992310+bitsko@users.noreply.github.com>
2022-07-25 01:59:20 -0700

    Update grab_uthash.sh

--- grab_uthash.sh
+++ grab_uthash.sh
@@ -1,1 +1,2 @@
 wget -q --show-progress https://raw.githubusercontent.com/troydhanson/uthash/master/src/{utarray.h,uthash.h,utlist.h,utringbuffer.h,utstack.h,utstring.h}
+sudo cp {utarray.h,uthash.h,utlist.h,utringbuffer.h,utstack.h,utstring.h} /usr/local/include
